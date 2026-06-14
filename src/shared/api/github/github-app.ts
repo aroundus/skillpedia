9a9dc18ef0c96d80a73708ef670d68201dad8b66
백은주 <specialguest@kakao.com>
2026-06-14 13:43:40 +0900

    feat: shared 레이어 추가

---
+++ src/shared/api/github/github-app.ts
@@ -0,0 +1,24 @@
+import { App } from 'octokit';
+
+const app = new App({
+  appId: process.env.GITHUB_APP_ID!,
+  privateKey: process.env.GITHUB_PRIVATE_KEY!,
+});
+
+export const getInstallationOctokit = async (owner: string, repo: string) => {
+  try {
+    // 1. App 권한(JWT)으로 해당 저장소에 앱이 설치된 정보를 가져옵니다.
+    const { data: installation } = await app.octokit.rest.apps.getRepoInstallation({
+      owner,
+      repo,
+    });
+
+    // 2. 찾아낸 Installation ID를 사용하여 해당 저장소에 접근 가능한 Octokit 인스턴스를 반환합니다.
+    return await app.getInstallationOctokit(installation.id);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const githubApp = app;
---
+++ src/shared/api/index.ts
@@ -0,0 +1,1 @@
+export { getInstallationOctokit, githubApp } from './github';
